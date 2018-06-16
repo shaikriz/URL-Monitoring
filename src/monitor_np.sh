5a4f3fa02d3fd0f154648342a4bc79bf56bdf50f
shaikriz <compriz@gmail.com>
2018-06-16 23:52:19 +0530

    correcting errors for shellcheck

--- src/monitor_np.sh
+++ src/monitor_np.sh
@@ -13,23 +13,38 @@ fi
 while read -r line
 do
 sleep 3
-status=`curl -Is  $line | head -1 | cut -d . -f 2 | cut -c 3-5`
-sleep 3
+status=$(curl -Is  $line | head -1 | cut -d . -f 2 | cut -c 3-5)
+curl -Is "${array[i]}" 1>>/dev/null
+exit_status=$?
+
+# Retry 3 times if the url is not accessible
+if [[ "${exit_status}"  != 0 ]]
+then
+until [[ $a == 3 ]]
+do
+((a++))
+echo -e "\nConnecting to ${array[i]} ..."
+curl -Is "${array[i]}" 1>>/dev/null
+done
+echo -e "\nConnectivity failure: $status"
+echo "Please check the url/format: ${array[i]}"
+status="NA"
+fi
 time=$(date +"%s")
 response=$(curl -Is -w "%{time_total}"\\n -o /dev/null "$line")
 
 #Check the status code and assign description accordingly
 if [[ $status == *200* ]]
 then
-echo $line , $status , ComponentStatus:GREEN , $time , $response >> output.txt
+echo "$line" , "$status" , ComponentStatus:GREEN , "$time" , $response >> output.txt
 else 
-echo  $line , $status , NotAccessible , $time , $response >> output.txt
+echo  "$line" , "$status" , NotAccessible , $time , "$response" >> output.txt
 
 #Print the table with values
 printf '%-*s%-*s%*s%*s%*s%*s\n' "$urlwidth" URL "$statuswidth" Status "$descwidth" Description "$descwidth" Timestamp "$descwidth" ResponseTime
-while IFS=, read URL Status Description Timestamp ResponseTime
+while IFS=, read -r URL Status Description Timestamp ResponseTime
 do
-    printf '%-*s%-*s%*s%*s%*s%*s%*s\n' "$urlwidth" $URL "$statuswidth" $Status "$descwidth" $Description "$descwidth" $Timestamp "$descwidth" $ResponseTime
+    printf '%-*s%-*s%*s%*s%*s%*s%*s\n' "$urlwidth" "$URL" "$statuswidth" "$Status" "$descwidth" "$Description" "$descwidth" "$Timestamp" "$descwidth" "$ResponseTime"
 done < output.txt
 fi
 done < src/input.txt
