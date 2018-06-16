cd11c2ef8ad62950419e58fbbb8ec8345dc3304c
shaikriz <compriz@gmail.com>
2018-06-17 00:25:25 +0530

    Adding test file checkformat.sh

---
+++ tests/checkformat.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Test to check if the given url format is valid
+
+regex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
+string='https://www.google.com/test/link.php'
+
+if [[ ${string} =~ ${regex} ]]
+then
+ecode=$?
+echo $ecode
+#assertEquals "0" "$?"
+ echo "Link valid"
+else
+    echo "Link not valid"
+echo $ecode
+fi
+
+function testformat () {
+
+assertEquals "0" "${ecode}"
+
+}
+
+. shunit2-2.1.6/src/shunit2
