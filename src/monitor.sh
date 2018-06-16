66e029badafec0d2b9082eb468221d79a5bbe24b
shaikriz <compriz@gmail.com>
2018-06-16 23:24:31 +0530

    Removing shellcheck errors

--- src/monitor.sh
+++ src/monitor.sh
@@ -6,10 +6,7 @@ array=(${url//,/ })
 
 #Formatting for Output Table
 
-headformat='%-*s%-*s%*s%*s%*s%*s\n'
-headformate='%-*f%-*f%*f%*f\n'
-format='%-*s%-*s%*s%*s%*s%*s%*s\n'
-modwidth=16; descwidth=24; qtywidth=6; pricewidth=10
+urlwidth=38; modwidth=16; descwidth=24
 a=0
 
 # Delete output file to remove old data
@@ -18,23 +15,19 @@ then
 rm output.txt
 fi
 
-#printf "\n$headformat" "$descwidth" URL "$descwidth" Status "$descwidth" Description "$descwidth" Timestamp "$descwidth" ResponseTime | tee result.txt
-
-
+#Function to print table with values
 print () {
 while IFS=, read URL Status Description Timestamp ResponseTime
 do
-    printf "$format" "$descwidth" $URL "$descwidth" $Status "$descwidth" $Description "$descwidth" $Timestamp "$descwidth" $ResponseTime
+    printf '%-*s%-*s%*s%*s%*s%*s%*s\n' "$urlwidth" $URL "$descwidth" $Status "$descwidth" $Description "$descwidth" $Timestamp "$descwidth" $ResponseTime
 done < output.txt
 }
-
-
 
 #Run the urls stored in the array to test connectivity and provide http code and response time
 for i in "${!array[@]}"
 do
 
-status=$(curl -Is -w %{http_code}\\n -o /dev/null ${array[i]})
+status=$(curl -Is -w "%{http_code}"\\n -o /dev/null ${array[i]})
 curl -Is ${array[i]} 1>>/dev/null
 
 # Retry 3 times if the url is not accessible
@@ -44,27 +37,25 @@ until [[ $a == 3 ]]
 do
 ((a++))
 echo -e "\nConnecting to ${array[i]} ..."
-curl -Is ${array[i]} 1>>/dev/null
+curl -Is "${array[i]}" 1>>/dev/null
 done
 echo -e "\nConnectivity failure: $status"
 echo "Please check the url/format: ${array[i]}"
 status="NA"
 fi 
 time=$(date +"%s")
-#echo $output
-response=$(curl -Is -w %{time_total}\\n -o /dev/null ${array[i]})
-#echo $response
-#echo $status
-#echo $time
+
+response=$(curl -Is -w %{time_total}\\n -o /dev/null "${array[i]}")
 
 #Check the status of connectivity and assign description accordingly
 if [[ $status == *200* ]]
 then
-echo ${array[i]} , $status , ComponentStatus:GREEN , $time , $response >> output.txt
+echo "${array[i]}" , $status , ComponentStatus:GREEN , $time , $response >> output.txt
 else 
-echo  ${array[i]} , $status , NotAccessible , $time , $response >> output.txt
+echo  "${array[i]}" , $status , NotAccessible , $time , $response >> output.txt
 fi
 done
+
 #Print the table by parsing the values stored in output file
-printf "\n$headformat" "$descwidth" URL "$descwidth" Status "$descwidth" Description "$descwidth" Timestamp "$descwidth" ResponseTime | tee result.txt
+printf '%-*s%-*s%*s%*s%*s%*s\n' "$descwidth" URL "$descwidth" Status "$descwidth" Description "$descwidth" Timestamp "$descwidth" ResponseTime | tee result.txt
 print
