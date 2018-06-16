e38602539ea684241ab28b7d8621a042146d71ff
shaikriz <compriz@gmail.com>
2018-06-16 23:31:00 +0530

    correcting shellcheck errors

--- src/monitor_np.sh
+++ src/monitor_np.sh
@@ -1,41 +1,35 @@
 #!/bin/bash
-
-array=(${url//,/ })
-
-headformat='%-*s%-*s%*s%*s%*s%*s\n'
-headformate='%-*f%-*f%*f%*f\n'
-format='%-*s%-*s%*s%*s%*s%*s%*s\n'
+#This is a script for Monitoring urls with no external parameters. The script accepts urls from file. Update src/input.txt with urls
+#and run the script
 
 urlwidth=38; descwidth=24; statuswidth=15
-
-#while read LINE
-#    do output=`curl -Is $LINE | head -1`
 
 if [ -f output.txt ]
 then
 rm output.txt
 fi
 
-#for i in "${!array[@]}"
-#do
-#echo "$i=>${array[i]}"
-while read line
+#Read the input file line by line and process
+while read -r line
 do
 sleep 3
 status=`curl -Is  $line | head -1 | cut -d . -f 2 | cut -c 3-5`
 sleep 3
 time=$(date +"%s")
-response=$(curl -Is -w %{time_total}\\n -o /dev/null $line)
+response=$(curl -Is -w "%{time_total}"\\n -o /dev/null "$line")
+
+#Check the status code and assign description accordingly
 if [[ $status == *200* ]]
 then
 echo $line , $status , ComponentStatus:GREEN , $time , $response >> output.txt
 else 
 echo  $line , $status , NotAccessible , $time , $response >> output.txt
-printf "$headformat" "$urlwidth" URL "$statuswidth" Status "$descwidth" Description "$descwidth" Timestamp "$descwidth" ResponseTime
+
+#Print the table with values
+printf '%-*s%-*s%*s%*s%*s%*s\n' "$urlwidth" URL "$statuswidth" Status "$descwidth" Description "$descwidth" Timestamp "$descwidth" ResponseTime
 while IFS=, read URL Status Description Timestamp ResponseTime
 do
-    printf "$format" "$urlwidth" $URL "$statuswidth" $Status "$descwidth" $Description "$descwidth" $Timestamp "$descwidth" $ResponseTime
+    printf '%-*s%-*s%*s%*s%*s%*s%*s\n' "$urlwidth" $URL "$statuswidth" $Status "$descwidth" $Description "$descwidth" $Timestamp "$descwidth" $ResponseTime
 done < output.txt
 fi
-#done
 done < src/input.txt
